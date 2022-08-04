b2f0df637c3a12d701cbbeaee1798027c4adfae9
PaStar98 <startekp98@gmail.com>
2022-08-04 09:32:30 +0200

    Minor changes

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,18 @@
+import { Fragment } from 'react';
 import styles from './Layout.module.css';
 
-const Layout = props => {
-  return <main className={styles.main}>{props.children}</main>;
+const Layout = ({ children, darkModeData }) => {
+  let theme;
+  darkModeData ? (theme = 'dark-mode') : (theme = 'light-mode');
+
+  return (
+    <Fragment>
+      <main className={theme}>
+        <div className={styles['main-container']}>{children}</div>
+      </main>
+      ;
+    </Fragment>
+  );
 };
 
 export default Layout;
