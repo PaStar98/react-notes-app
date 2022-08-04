3941e5d222f6de75442294c907406013b0b640f2
PaStar98 <startekp98@gmail.com>
2022-08-04 09:33:13 +0200

    Add: handle delete notes

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,12 +1,20 @@
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = ({ text, date }) => {
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  const handleDelete = () => {
+    handleDeleteNote(id);
+  };
+
   return (
     <div className={'note'}>
       <span>{text}</span>
       <div className={'note-footer'}>
         <small>{date}</small>
-        <MdDeleteForever size={'1.3rem'} />
+        <MdDeleteForever
+          size={'1.3rem'}
+          className={'delete-btn'}
+          onClick={handleDelete}
+        />
       </div>
     </div>
   );
--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -2,11 +2,17 @@ import Note from './Note';
 import AddNote from './AddNote';
 import styles from './NoteList.module.css';
 
-const NoteList = ({ notesData, handleAddNote }) => {
+const NoteList = ({ notesData, handleAddNote, handleDeleteNote }) => {
   return (
     <section className={styles['note-list']}>
       {notesData.map(note => (
-        <Note key={note.id} text={note.text} date={note.date} />
+        <Note
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
       ))}
       <AddNote handleAddNote={handleAddNote} />
     </section>
